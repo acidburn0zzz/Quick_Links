2b8d96b2cc5d0a76bfb85ea62cc7810f1f3fcd50
anubhavagwl <anubhav.agwl@gmail.com>
2015-05-19 18:10:13 +0530

    Quick Links Generator
    Google Chrome Extension which gets current website url in the form of
    QR code helpful for mobile devices

---
+++ popup.js
@@ -0,0 +1,51 @@
+function makeShort(urlpass) 
+{
+   var longUrl=urlpass;
+    var request = gapi.client.urlshortener.url.insert({
+      'resource': {
+      'longUrl': longUrl
+    }
+    });
+    request.execute(function(response) 
+    {
+        var shorturl=response.id;
+        
+ 
+    });
+    return shorturl;
+ }
+ 
+function load()
+{
+    //Get your own Browser API Key from  https://code.google.com/apis/console/
+    gapi.client.setApiKey('AIzaSyBpQ06-kI-C2E688-nZ8Lstt733XxD3UoM');
+    gapi.client.load('urlshortener', 'v1',function(){document.getElementById("output").innerHTML="";});
+ 
+}
+
+window.onload = load;
+
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    
+    document.getElementById("demo").innerHTML=makeShort(url);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
